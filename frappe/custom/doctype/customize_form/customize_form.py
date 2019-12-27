e5e03021c069a75aea889da8eb605508e98cf9d6
Himanshu Warekar <himanshuwarekar@yahoo.com>
2019-12-27 12:31:24 +0530

    fix: allow quick entry via customize form

--- frappe/custom/doctype/customize_form/customize_form.py
+++ frappe/custom/doctype/customize_form/customize_form.py
@@ -31,7 +31,8 @@ doctype_properties = {
 	'track_changes': 'Check',
 	'track_views': 'Check',
 	'allow_auto_repeat': 'Check',
-	'allow_import': 'Check'
+	'allow_import': 'Check',
+	'allow_in_quick_entry': 'Check'
 }
 
 docfield_properties = {
@@ -68,7 +69,8 @@ docfield_properties = {
 	'columns': 'Int',
 	'remember_last_selected_value': 'Check',
 	'allow_bulk_edit': 'Check',
-	'auto_repeat': 'Link'
+	'auto_repeat': 'Link',
+	'allow_in_quick_entry': 'Check'
 }
 
 allowed_fieldtype_change = (('Currency', 'Float', 'Percent'), ('Small Text', 'Data'),
