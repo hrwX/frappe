2debabd4a1f6737da4c41e7a8cead2d8d52e8bd2
hrwx <himanshuwarekar@yahoo.com>
2020-10-30 15:30:01 +0530

    fix: just fetch from cache!

--- frappe/integrations/oauth2_logins.py
+++ frappe/integrations/oauth2_logins.py
@@ -38,9 +38,10 @@ def login_via_fairlogin(code, state):
 @frappe.whitelist(allow_guest=True)
 def get_login_url(redirect_to=None, social_login_key_name="bloomstack"):
 
-	if not redirect_to and frappe.get_hooks("bloomstack_client_id") and frappe.get_hooks("bloomstack_redirect_uri"):
+	if not redirect_to and frappe.get_cached_value("OAuth Client", {"app_name": "Bloomstack"}, "client_id") and frappe.get_hooks("bloomstack_redirect_uri"):
 		redirect_to = "{0}/api/method/frappe.integrations.oauth2.authorize?client_id={1}&response_type=code&scope=openid&prompt=none& \
-			redirect_uri={2}".format(frappe.utils.get_url(), frappe.get_hooks("bloomstack_client_id"), frappe.get_hooks("bloomstack_redirect_uri"))
+			redirect_uri={2}".format(frappe.utils.get_url(), frappe.get_cached_value("OAuth Client", {"app_name": "Bloomstack"}, "client_id"),
+			frappe.get_hooks("bloomstack_redirect_uri"))
 
 	redirect_to = redirect_to.replace("&amp;", "&")
 	return get_oauth2_authorize_url(social_login_key_name, redirect_to)
