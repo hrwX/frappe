c1c78049c2c562acbefa0c92cba803829aabe3fc
hrwx <himanshuwarekar@yahoo.com>
2020-11-02 11:56:08 +0530

    chore: rename to default_redirect_uri

--- frappe/integrations/oauth2_logins.py
+++ frappe/integrations/oauth2_logins.py
@@ -41,7 +41,7 @@ def get_login_url(redirect_to=None, social_login_key_name="bloomstack"):
 	client = frappe.get_cached_doc("OAuth Client", {"app_name": "Frappe"})
 	if not redirect_to and client:
 		redirect_to = "{0}/api/method/frappe.integrations.oauth2.authorize?client_id={1}&response_type=code&scope=openid&prompt=none& \
-			redirect_uri={2}".format(frappe.utils.get_url(), client.get("client_id"), client.get("redirect_uris"))
+			redirect_uri={2}".format(frappe.utils.get_url(), client.get("client_id"), client.get("default_redirect_uri"))
 
 	redirect_to = redirect_to.replace("&amp;", "&")
 	return get_oauth2_authorize_url(social_login_key_name, redirect_to)
